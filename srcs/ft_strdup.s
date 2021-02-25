969f915a0bc4b6b20e016166855630437165f310
Sagow <marina.delwaulle@free.fr>
2021-02-25 16:01:10 +0100

    ft_strdup fin

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -3,14 +3,22 @@
 			extern	ft_strlen, malloc, ft_strcpy
 
 ft_strdup:				;ft_strdup(str) = ft_strdup(rdi)
+			cmp rdi, 0
+			je _failure
 			push rdi
 			call ft_strlen
 			pop rdi
 			push rdi
 			mov rdi, rax
 			call malloc wrt ..plt
+			cmp rax, 0
+			je _failure
 			pop rdi
 			mov rsi, rdi
 			mov rdi, rax
 			call ft_strcpy
+			ret
+
+_failure:
+			mov rax, 0
 			ret
